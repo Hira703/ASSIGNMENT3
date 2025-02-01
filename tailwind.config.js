3fa719b788d9ec3d9fbc1265fd320e365abec5ea
Hira703 <soniahira48@gmail.com>
2025-02-01 21:04:40 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html", // if you have an index.html file
+      "./src/**/*.{js,ts,jsx,tsx,html}", // Tailwind will look for classes in JS, TS, JSX, TSX, and HTML files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
